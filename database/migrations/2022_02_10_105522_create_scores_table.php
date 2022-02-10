6742be1145fe3908d457a54c882d70613a5e0330
Suryo Mujahid <suryomujahid@gmail.com>
2022-02-10 18:03:09 +0700

    forgot save the file in staging

--- database/migrations/2022_02_10_105522_create_scores_table.php
+++ database/migrations/2022_02_10_105522_create_scores_table.php
@@ -14,7 +14,20 @@ class CreateScoresTable extends Migration
     public function up()
     {
         Schema::create('scores', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->uuid('subject_id');
+            $table->integer('ph1');
+            $table->integer('ph2');
+            $table->integer('pts_ganjil');
+            $table->integer('ph3');
+            $table->integer('ph4');
+            $table->integer('pas_ganjil');
+            $table->integer('ph5');
+            $table->integer('ph6');
+            $table->integer('pas_genap');
+            $table->integer('ph7');
+            $table->integer('ph8');
+            $table->integer('pat');
             $table->timestamps();
         });
     }
