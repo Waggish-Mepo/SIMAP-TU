cfa6b4f2242efea6659c02bb31fdd94eaa056133
FandiA6 <fandiahmad3556@gmail.com>
2022-02-21 10:50:10 +0700

    feat(Login) : Responsive Mobile Login

--- resources/views/layout/login.blade.php
+++ resources/views/layout/login.blade.php
@@ -45,7 +45,7 @@
                   </div>
                 </div>
 
-                <button type="submit" class="px-5 py-2 text-sm font-medium text-center text-white bg-blue-900 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 w-72 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 xl:px-0 xl:h-11 xl:text-base sm:h-16 sm:px-72 sm:text-2xl">Login</button>
+                <button type="submit" class="px-5 py-2 text-sm font-medium text-center text-white bg-blue-900 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 w-72 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 xl:px-0 xl:h-11 xl:text-base sm:h-16 sm:px-72 sm:text-2xl ">Login</button>
             </form>
         </div>
     </div>
