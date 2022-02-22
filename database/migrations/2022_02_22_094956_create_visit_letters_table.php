ce20efaa6a5c857a9373d16bb084a65dc47d8124
Saekyo <reskijunaedisalat1@gmail.com>
2022-02-22 17:11:39 +0700

    feat(migration) : Add visit_letters & visit_documentaions migrations

---
+++ database/migrations/2022_02_22_094956_create_visit_letters_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitLettersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visit_letters', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->numeric('no_surat');
+            $table->string('lampiran');
+            $table->string('perihal');
+            $table->string('kepada');
+            $table->string('hari');
+            $table->date('waktu');
+            $table->string('tempat');
+            $table->integer('jumlah_peserta');
+            $table->string('keterangan');
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visit_letters');
+    }
+}
