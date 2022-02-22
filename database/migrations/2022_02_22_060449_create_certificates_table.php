b09058a0f31e5794c1a783c0be2ea0f4c88d8076
Saekyo <reskijunaedisalat1@gmail.com>
2022-02-22 16:38:29 +0700

    feat(migration) : Add certificates migrations

---
+++ database/migrations/2022_02_22_060449_create_certificates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCertificatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('employee_id');
+            $table->string('nama_kegiatan');
+            $table->date('tgl_kegiatan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certificates');
+    }
+}
