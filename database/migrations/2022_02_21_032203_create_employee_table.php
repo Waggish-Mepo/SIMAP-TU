ffe875f52956876d0f71daadb1241b06c57b13af
Azriel Fauzi Hermansyah <55310608+Azrielfhr2@users.noreply.github.com>
2022-02-26 10:32:40 +0700

    Update migration employee

--- database/migrations/2022_02_21_032203_create_employee_table.php
+++ database/migrations/2022_02_21_032203_create_employee_table.php
@@ -17,6 +17,7 @@ class CreateEmployeeTable extends Migration
             $table->uuid('id')->primary();
             $table->string('nama')->nullable();
             $table->string('status_pegawai')->nullable();
+            $table->string('alamat')->nullable();
             $table->integer('nip')->nullable();
             $table->integer('nigk')->nullable();
             $table->string('jenis_ptk')->nullable();
@@ -30,9 +31,8 @@ class CreateEmployeeTable extends Migration
             $table->integer('kartu_pegawai')->nullable();
             $table->integer('kartu_suami')->nullable();
             $table->integer('kartu_istri')->nullable();
-            $table->string('ijazah')->nullable();
-            $table->string('ktp')->nullable();
-            $table->string('kk')->nullable();
+            $table->integer('ktp')->nullable();
+            $table->integer('kk')->nullable();
             $table->timestamps();
         });
     }
