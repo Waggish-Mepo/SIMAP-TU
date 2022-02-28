c4ff372884429062a0121f5f325705023c8fa129
Suryo Mujahid <suryomujahid@gmail.com>
2022-02-28 23:33:42 +0700

    feat(view): add menu item for each role, and new navbar

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -3,3 +3,4 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="icon" href="{{asset('img/svg/icon.svg')}}" type="image/x-icon">
 <link href="/css/app.css" rel="stylesheet">
+<script src="https://kit.fontawesome.com/6b35a228d3.js" crossorigin="anonymous"></script>
