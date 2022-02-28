aff5edf3f839beb6ee8c51d05aa6a322c1d4596e
Azriel Fauzi Hermansyah <55310608+Azrielfhr2@users.noreply.github.com>
2022-02-28 20:24:21 +0700

    delete fillable on model meeting

--- app/Models/Meeting.php
+++ app/Models/Meeting.php
@@ -12,17 +12,5 @@ class Meeting extends Model
     protected $table = 'meetings';
 
     protected $incrementing = false;
-
-    protected $fillable = [
-        'id',
-        'notula_id',
-        'pimpinan_rapat',
-        'materi',
-        'daftar_hadir',
-        'tanggal',
-        'waktu',
-        'tempat',
-        'status',
-        'dokumentasi'
-    ];
+    
 }
