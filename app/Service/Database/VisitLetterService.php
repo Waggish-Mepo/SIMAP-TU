e302d7345bffbe4392486cf740c20a84435f31c5
Saekyo <reskijunaedisalat1@gmail.com>
2022-03-01 19:39:44 +0700

    feat(Service) : Add kunjungan visit_letter service

---
+++ app/Service/Database/VisitLetterService.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Service\Database;
+
+use App\Models\VisitLetter;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
+use Ramsey\Uuid\Uuid;
+
+class VisitLetterService{
+
+    public function index($filter = [])
+    {
+        $orderBy = $filter['order_by'] ?? 'DESC';
+        $per_page = $filter['per_page'] ?? 99;
+        $no_surat = $filter['no_surat'] ?? null;
+        $hari = $filter['hari'] ?? null;
+        $status = $filter['status'] ?? null;
+
+        $query = VisitLetter::orderBy('created_at', $orderBy);
+
+        if ($no_surat !== null) {
+            $query->where('no_surat', $no_surat);
+        }
+
+        if ($hari !== null) {
+            $query->where('hari', $hari);
+        }
+
+        if ($status !== null) {
+            $query->where('status', $status);
+        }
+
+        $visit_letter = $query->simplePaginate($per_page);
+
+        return $visit_letter->toArray();
+    }
+
+    public function detail($visitLetterId)
+    {
+        $visit_letter = VisitLetter::findOrFail($visitLetterId);
+
+        return $visit_letter->toArray();
+    }
+
+    public function create($payload)
+    {
+        $visit_letter = new VisitLetter;
+        $visit_letter->id = Uuid::uuid4()->toString();
+        $visit_letter = $this->fill($visit_letter, $payload);
+        $visit_letter->save();
+
+        return $visit_letter;
+    }
+
+    public function update($visitLetterId, $payload)
+    {
+        $visit_letter = VisitLetter::findOrFail($visitLetterId);
+        $visit_letter = $this->fill($visit_letter, $payload);
+        $visit_letter->save();
+
+        return $visit_letter->toArray();
+    }
+
+    private function fill(VisitLetter $visit_letter, array $attributes)
+    {
+        foreach ($attributes as $key => $value) {
+            $visit_letter->$key = $value;
+        }
+
+        $validate = Validator::make($visit_letter->toArray(), [
+            'no_surat' => 'string',
+            'lampiran' => 'string',
+            'perihal' => 'string',
+            'kepada' => 'string',
+            'hari' => 'string',
+            'waktu' => 'date',
+            'tempat' => 'string',
+            'jumlah_peserta' => 'integer',
+            'keterangan' => 'string',
+            'status' => 'string'
+        ]);
+
+        if($validate->fails()) {
+            return $validate->errors()->toArray();
+        }
+
+        return $visit_letter;
+    }
+}
