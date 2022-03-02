879bba892f1972fca206b0f93cca9fda897ff08a
Azriel Fauzi Hermansyah <azrielfhr23@gmail.com>
2022-03-02 12:09:36 +0700

    feat(migration & Model) : Add migratin & model surat menyurat

---
+++ app/Models/LetterOut.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LetterOut extends Model
+{
+    use HasFactory;
+
+    // sifat surat
+    const Rahasia = 'Rahasia';
+    const Penting = 'Penting';
+    const Segera = 'Segera';
+    const Biasa = 'Biasa';
+
+    protected $incrementing = false;
+}
